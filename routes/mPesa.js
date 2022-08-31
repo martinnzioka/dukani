ce6c42cc8d7316ae4935c6161b2a639754d1bfaf
martinnzioka <martin.mutua@protonmail.com>
2022-08-31 16:27:44 +0300

    31-08-2022: Updated deleted files

--- routes/mPesa.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/m-pesa', function(req, res, next) {
-  res.json(req.mPesaAuth['access_token']);
-});
-
-module.exports = router;
